70e2edad7771fcf70ab1071b658c5fa5b058276a
David <davidgb@galicia.com>
2024-10-26 00:06:40 +0200

    sqrt done y tested

--- app/src/main/java/aed/recursion/Utils.java
+++ app/src/main/java/aed/recursion/Utils.java
@@ -2,7 +2,6 @@ package aed.recursion;
 
 
 import es.upm.aedlib.Pair;
-import es.upm.aedlib.Position;
 import es.upm.aedlib.positionlist.*;
 
 
@@ -10,7 +9,15 @@ import es.upm.aedlib.positionlist.*;
 public class Utils {
 
   public static int sqrt(int n) {
-    return 2;
+    return sqrtRec(n, 1, n);
+  }
+
+  private static int sqrtRec(int n, int start, int end) {
+    int mitad = start + (end - start) / 2; //no hago (start + end)/2 para evitar overflow con numeros grandes
+    if((mitad*mitad <= n && (mitad+1)*(mitad+1) > n)) return mitad;
+    if(mitad*mitad > n) return sqrtRec(n, start, mitad-1);
+    return sqrtRec(n, mitad+1, end);
+
   }
 
   public static Iterable<Integer> primes(int n) {
