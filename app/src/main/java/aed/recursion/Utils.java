7f10607e942bafa167e7cfe8aca2bc7293db52a6
David <davidgb@galicia.com>
2024-10-25 17:44:47 +0200

    Initial commit

---
+++ app/src/main/java/aed/recursion/Utils.java
@@ -0,0 +1,24 @@
+package aed.recursion;
+
+
+import es.upm.aedlib.Pair;
+import es.upm.aedlib.Position;
+import es.upm.aedlib.positionlist.*;
+
+
+
+public class Utils {
+
+  public static int sqrt(int n) {
+    return 2;
+  }
+
+  public static Iterable<Integer> primes(int n) {
+    return null;
+  }
+
+  public static <E> boolean equals(PositionList<Pair<E,Integer>> p1, PositionList<Pair<E,Integer>> p2) {
+    return false;
+  }
+
+}
